da47e32bc747dad3adbdddb021faab92a961cf2c
MrHunterBlood <efr.munoz@duocuc.cl>
2024-06-10 17:23:24 -0400

    V4
    Mejoras

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@
 $(document).ready(function() {
   $('#animar').click(function() {
   $('#brazo-izquierdo').css({ left: '130px', top: '380px'  });
-  $('#brazo-derecho').css({ left: '35px', top: '380px'  });
+  $('#brazo-derecho').css({ left: '260px', top: '380px'  });
   $('#ojo-izquierdo').css({ left: '165px', top: '235px' });
   $('#ojo-derecho').css({ left: '266px', top: '233px'  });
   $('#boca').css({ top: '300px', left: '185px' });
@@ -14,7 +14,7 @@ $(document).ready(function() {
 
 $(document).ready(function() {
   $('#animar2').click(function() {
-  $('#brazo-derecho').css({ 'transform': 'rotate(200deg)'});
+  $('#brazo-derecho').css({ 'transform': 'rotate(175deg)'});
 
 });
 });
