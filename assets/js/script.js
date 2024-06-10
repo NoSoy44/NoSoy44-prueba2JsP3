118026071131c98a786f533c0aa38da3cc887ead
MrHunterBlood <efr.munoz@duocuc.cl>
2024-06-10 13:38:45 -0400

    Pikachu
    V3

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,17 @@ $(document).ready(function() {
   $('#animar').click(function() {
   $('#brazo-izquierdo').css({ left: '130px', top: '380px'  });
   $('#brazo-derecho').css({ left: '35px', top: '380px'  });
-  $('#ojo-izquierdo').css({ left: '150px', top: '215px' });
-  $('#ojo-derecho').css({ left: '245px', top: '210px'  });
-  $('#boca').css({ top: '300px', left: '166px' });
+  $('#ojo-izquierdo').css({ left: '165px', top: '235px' });
+  $('#ojo-derecho').css({ left: '266px', top: '233px'  });
+  $('#boca').css({ top: '300px', left: '185px' });
   $('#nariz').css({ top: '180px', left: '90px' });
   $('#cola').css({ top: '200px', left: '333px' });
+});
+});
+
+$(document).ready(function() {
+  $('#animar2').click(function() {
+  $('#brazo-derecho').css({ 'transform': 'rotate(200deg)'});
+
 });
 });
